8290d154779a8408de610f8f0bbbf53f932e5975
Jacob Orr <jacobalexanderorr@gmail.com>
2025-09-05 11:04:51 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'org.jacoborr.ttc',
-  appName: 'My-TTC UI',
-  webDir: 'www'
-};
-
-export default config;
